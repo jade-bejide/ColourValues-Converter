2ee23c23608d40d2a139f6ad0284577e98d256cc
chwestpains <59064406+chwestpains@users.noreply.github.com>
2020-05-05 11:48:57 +0100

    Update RGBToHex.py

--- RGBToHex.py
+++ RGBToHex.py
@@ -1,10 +1,12 @@
 class ColourValues:
     def __init__(self, r, g, b):
+        #records the decimal r, g and b values
         self.r = r
         self.g = g
         self.b = b
-        self.hex_equivalent = ""
-        self.rgb_equivalent = (self.r, self.g, self.b)
+        #initialises the colour values (RGB and hex equivalent) as null
+        self.hex_equivalent = None
+        self.rgb_equivalent = None
 
     def get_r(self):
         return self.r
@@ -22,6 +24,7 @@ class ColourValues:
         return self.convertToRGB()
 
     def convertToLetter(self, value):
+        #switch case implementation to select letters corresponding to the numerical hex value
         if value == 10:
             return "A"
         elif value == 11:
@@ -36,6 +39,7 @@ class ColourValues:
             return "F"
 
     def convertToNumber(self, value):
+        #switch case implementation to select numbers corresponding to the alphabet hex value
         if value == "A":
             return 10
         elif value == "B":
@@ -51,17 +55,24 @@ class ColourValues:
         else:
             return value
 
+    def convertSectionsToHex(self, value):
+        #stores the quotient 16 value
+        first = value // 16
+        #stores the modulus 16 value
+        second = value % 16
+        #if the two variables are more than 9, then the variables are converted to their alphabet equvalent
+        if first > 9:
+            first = self.convertToLetter(first)
+        if second > 9:
+            second = self.convertToLetter(second)
 
-    def convertSectionsToRGB(self, value):
-        number = 0
-        first = value[1]
-        first = self.convertToNumber(first)
-        second = self.convertToNumber(second)
-        number = first * 16
-        number += second
-        
+        first = str(first)
+        second = str(second)
+
+        return first + second
 
     def convertToRGB(self):
+        #splits the hex_equivalent attributes to single digits
         first = self.hex_equivalent[1]
         second = self.hex_equivalent[2]
         third = self.hex_equivalent[3]
@@ -69,9 +80,11 @@ class ColourValues:
         fifth = self.hex_equivalent[5]
         sixth = self.hex_equivalent[6]
 
+        #decimal values are found
         newR = int(self.convertToNumber(first)) * 16 + int(self.convertToNumber(second))
         newG = int(self.convertToNumber(third)) * 16 + int(self.convertToNumber(fourth))
         newB = int(self.convertToNumber(fifth)) * 16 + int(self.convertToNumber(sixth))
+        #result is stored as a tuple
         RGBvalue = (newR, newG, newB)
 
         return RGBvalue
@@ -79,9 +92,11 @@ class ColourValues:
         
 
     def convertToHex(self):
+        #converts r, g, b values from decimal to hex
         value1 = self.convertSectionsToHex(self.r)
         value2 = self.convertSectionsToHex(self.g)
         value3 = self.convertSectionsToHex(self.b)
+        #sets hex_equivalent attribute to its corresponding hex value
         self.hex_equivalent = "#" + value1 + value2 + value3
 
         return self.hex_equivalent
